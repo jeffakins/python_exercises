a886bf5f5f63666297fd117c03e6b5c0d0390c60
Jeff Akins <jeffakins21@gmail.com>
2021-07-09 07:17:57 -0700

    Creation of first Jupyter notebook and partial completion of python intro exercises

--- python_intro_exercises.py
+++ python_intro_exercises.py
@@ -1,2 +1,24 @@
 greeting = 'Hello World'
 print(greeting)
+
+type(99.9)
+type(False)
+type("False")
+type(0)
+type("0")
+type(True)
+type('True')
+type([{}])
+type({'a': []})
+
+#Best Types for:
+#A term or phrase typed into a search box? -string
+#If a user is logged in? - bool
+#A discount amount to apply to a user's shopping cart? - int
+#Whether or not a coupon code is valid? - bool
+#An email address typed into a registration form? - string
+#The price of a product? - float
+#A Matrix? - list
+#The email addresses collected from a registration form? - str
+#Information about applicants to Codeup's data science program? dictionary
+
