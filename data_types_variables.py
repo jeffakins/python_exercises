cc05db1d0f7c15245576a4f76a355b2285ad5ecb
Jeff Akins <jeffakins21@gmail.com>
2021-07-09 13:05:56 -0700

    Updates to data types python exercises

--- data_types_variables.py
+++ data_types_variables.py
@@ -17,11 +17,11 @@ print("Total cost:", total_cost)
 # You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
 
 def google_weekly_pay(pay, hours): 
-    return pay * hours;
+    return pay * hours
 def Amazon_weekly_pay(pay, hours): 
-    return pay * hours;
+    return pay * hours
 def Facebook_weekly_pay(pay, hours): 
-    return pay * hours;
+    return pay * hours
 total_pay = google_weekly_pay(400, 6) + Amazon_weekly_pay(380, 4) + Facebook_weekly_pay(350, 10)
 print("Total weekly pay:", total_pay)
 
@@ -29,7 +29,7 @@ print("Total weekly pay:", total_pay)
 # and the class schedule does not conflict with her current schedule.
 
 def can_be_enrolled(class_openings, schedule_conflict):
-     return class_openings and schedule_conflict;
+     return class_openings and schedule_conflict
 print(can_be_enrolled(True, True))
 
 # A product offer can be applied only if people buys more than 2 items, 
@@ -37,5 +37,22 @@ print(can_be_enrolled(True, True))
 # Premium members do not need to buy a specific amount of products.
 
 def can_prod_offer_be_applied(items, expired, premium):
-    return items > 2 and expired == False or premium == True;
+    return items > 2 and expired == False or premium == True
 can_prod_offer_be_applied(3, False, True)
+
+
+#Create a variable that holds a boolean value for each of the following conditions:
+username = 'codeup'
+password = 'notastrongpassword'
+# the password must be at least 5 characters
+# the username must be no more than 20 characters
+# the password must not be the same as the username
+# bonus neither the username or password can start or end with whitespace
+def credentials(username, password):
+    length_password = len(password) >= 5
+    length_username = len(username) <= 20
+    password_username_notequal = username != password
+    whitespace_username = username != username.strip()
+    whitespace_password = password != password.strip()
+    results = length_password and length_username and password_username_notequal and not whitespace_password and not whitespace_username;
+print("Does the username and password meet the criteria?", credentials(username, password))
