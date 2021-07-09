0d809626fa51085e9652093fa4b65bd163205dcd
Jeff Akins <jeffakins21@gmail.com>
2021-07-09 08:06:59 -0700

    Complete with python intro exercises

--- python_intro_exercises.py
+++ python_intro_exercises.py
@@ -22,3 +22,26 @@ type({'a': []})
 #The email addresses collected from a registration form? - str
 #Information about applicants to Codeup's data science program? dictionary
 
+'1' + 2
+6 % 4
+type(5 % 4)
+type(type(6 % 4))
+'3 + 4 is ' + 3 + 4
+0 < 0
+'False' == False
+True == 'True'
+5 >= -5
+True or "42"
+6 % 5
+5 < 4 and  1 == 1
+'codeup' == 'codeup' and 'codeup' == 'Codeup'
+4 >= 0 and 1 != '1'
+6 % 3 == 0
+5 % 2 != 0
+[1] + 2
+[1] + [2]
+[1] * 2
+[1] * [2]
+[] + [] == []
+{} + {}
+
