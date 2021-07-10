eac45fcda5fe880dcec2bb11d6332ab79bc97ecb
Jeff Akins <jeffakins21@gmail.com>
2021-07-10 10:47:12 -0700

    Continues work on python control structures

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -25,7 +25,28 @@ print("The day of the week is", day_of_week)
 
 # c. create variables and make up values for
 #       - the number of hours worked in one week
+weekly_work_hours = 40
 #       - the hourly rate
+hourly_rate = pay / pay_period_hours
 #       - how much the week's paycheck will be
+weekly_paycheck = hourly_rate * weekly_work_hours
 #       - write the python code that calculates the weekly paycheck. 
+weekly_paycheck = hourly_rate * weekly_work_hours
 #         You get paid time and a half if you work more than 40 hours
+if weekly_work_hours > 40:
+    overtime_pay = overtime_hours * (hourly_rate * 1.5)
+    return overtime_pay
+
+
+# 2. Loop Basics
+#   a. While Loop
+#   - Create an integer variable i with a value of 5.
+i = 5
+#   - Create a while loop that runs so long as i is less than or equal to 15
+while i <= 15:
+    i += 1
+#   - Each loop iteration, output the current value of i, then increment i by one.
+i = 5
+while i <= 15:
+    print(i)
+    i += 1
