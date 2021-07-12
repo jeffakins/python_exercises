a562341f0b644a8e3bd7ca788a6522ffbeaf43b3
Jeff Akins <jeffakins21@gmail.com>
2021-07-11 21:54:13 -0700

    Control Structures exercises complete through question 3

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -78,24 +78,24 @@ while i >= 0:
 
 
 
-# For Loops
+# b. For Loops
 
-# - Write some code that prompts the user for a number, 
+# - 1. Write some code that prompts the user for a number, 
 #   then shows a multiplication table up through 10 for that number.
 print("Enter a number to show its multiplication table")
 number = int(input())
 for i in range(1, 11):
     print(number, "x", i, "=", number * i)
 
-# - Output number of number
+# - 2. Output number of number
 for i in range(1, 10):
     number = str(i)
     print(number * i)
 
 
-# break and continue
+# c. break and continue
 
-# - Prompt the user for an odd number between 1 and 50. 
+# - 1. Prompt the user for an odd number between 1 and 50. 
 # Use a loop and a break statement to continue prompting 
 # the user if they enter invalid input. 
 # (Hint: use the isdigit method on strings to determine this). 
@@ -112,3 +112,88 @@ if number.isdigit() == True and int(number) % 2 != 0 and int(number) > 0 and int
             print("Here is an odd number:", i)
 else:
     print("That is not an odd number between 1 and 50")
+
+
+# d. The input function can be used to prompt for input and use that input in your python code. 
+#    Prompt the user to enter a positive number and write a loop that counts from 0 to that number. 
+#    (Hints: first make sure that the value the user entered is a valid number, also note that 
+#    the input function returns a string, so you'll need to convert this to a numeric type.)
+
+print("Enter a positive integer")
+number = input()
+i = 0 # to initialize i
+if number.isdigit() == True and int(number) > 0: # to check that the input is a pos number
+    print("I will count to your number...")
+    while i <= int(number):
+        print(i)
+        i += 1
+else:
+    print("That is not a positive integer")
+
+# e. Write a program that prompts the user for a positive integer. 
+#    Next write a loop that prints out the numbers from the number the user entered down to 1.
+
+print("Enter a positive integer")
+number = input()
+i = int(number) # to initialize i
+if number.isdigit() == True and int(number) > 0: # to check that the input is a pos number
+    print("I will count from your number to 1...")
+    while i > 0:
+        print(i)
+        i -= 1
+else:
+    print("That is not a positive integer")
+
+
+# 3. Fizzbuzz
+
+# One of the most common interview questions for entry-level programmers is the FizzBuzz test. 
+# Developed by Imran Ghory, the test is designed to test basic looping and conditional logic skills.
+# 
+# Write a program that prints the numbers from 1 to 100.
+
+i = 1
+while i <= 100:
+    print(i)
+    i += 1
+
+# For multiples of three print "Fizz" instead of the number
+
+i = 1
+while i <= 100:
+    if i % 3 == 0:
+        print("Fizz")
+    else:
+        print(i)
+    i += 1
+
+# For the multiples of five print "Buzz"
+
+i = 1
+while i <= 100:
+    if i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+    i += 1
+
+# For numbers which are multiples of both three and five print "FizzBuzz"
+
+i = 1
+while i <= 100:
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 5 == 0:
+        print("Buzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    else:
+        print(i)
+    i += 1
+
+
+# 4. Display a table of powers.
+
+#    Prompt the user to enter an integer.
