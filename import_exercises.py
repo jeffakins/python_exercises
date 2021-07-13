4747b9cbbdcc3965330d814f548c01a8a71d2a0d
Jeff Akins <jeffakins21@gmail.com>
2021-07-13 12:33:21 -0700

    Start of the import exercises

---
+++ import_exercises.py
@@ -0,0 +1,15 @@
+# Import Exercises
+
+# 1. Import and test 3 of the functions from your functions exercise file. 
+#    Import each function in a different way:
+
+#    a. Run an interactive python session and import the module. 
+#       Call the is_vowel function using the . syntax.
+
+#    b. Create a file named import_exericses.py. 
+#       Within this file, use from to import the calculate_tip function directly. 
+#       Call this function with values you choose and print the result.
+
+#    c. Create a jupyter notebook named import_exercises.ipynb. 
+#       Use from to import the get_letter_grade function and give it an alias. 
+#       Test this function in your notebook.
