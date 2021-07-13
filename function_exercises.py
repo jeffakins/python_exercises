c6500e1d5d323d02cea6b351c03c3169da9a68ee
Jeff Akins <jeffakins21@gmail.com>
2021-07-13 09:00:31 -0700

    Problem 6 on funtion exercises

--- function_exercises.py
+++ function_exercises.py
@@ -18,17 +18,74 @@ print(is_two(4))
 # 2. Define a function named is_vowel. 
 #    It should return True if the passed string is a vowel, False otherwise.
 
-def is_vowel(letter):
+def is_vowel(letter): # Function to check if a letter is a vowel
+    """Frunction that will return True if the passed string is a vowel"""
     vowels = ["a", "e", "i", "o", "u"]
-    for letter in vowels:
-        if letter.lower() == vowels:
+    for vowel in vowels:
+        if letter.lower() == vowel:
             return True
-        else:
-            return False
+    return False
 
-print("Input a vowel")
+# User input. Will check if the input is a vowel using the above function.
+print("Input a vowel") 
 letter = input()
-if is_vowel() == True:
+if is_vowel(letter) == True:
     print("That is a vowel")
 else:
     print("Not a vowel")
+
+
+
+# 3. Define a function named is_consonant. 
+#    It should return True if the passed string is a consonant, False otherwise. 
+#    Use your is_vowel function to accomplish this.
+
+def is_consonant(letter): 
+    """Function that will return True if the passed string is a consonant"""
+    if is_vowel(letter) == False:
+        return True
+    else:
+        return False
+
+# User input. Will check if the input is a consonant using the above function.
+print("Input a letter") 
+letter = input()
+if is_consonant(letter) == True:
+    print("That is a consonant")
+else:
+    print("Not a consonant")
+
+
+# 4. Define a function that accepts a string that is a word. 
+#    The function should capitalize the first letter of the word if the word starts with a consonant.
+
+def cap_first_letter(word):
+    """Function that capitalies the first letter of a word if it's a consonant"""
+    if is_consonant(word[0]) == True:
+        return word.capitalize()
+    else:
+        return word
+
+# Testing that the above function works
+print("Type a word")
+word = input()
+print("Your word:")
+print(cap_first_letter(word))
+
+
+# 5. Define a function named calculate_tip. 
+#    It should accept a tip percentage (a number between 0 and 1) 
+#    and the bill total, and return the amount to tip.
+
+def calculate_tip(bill_total, tip_percentage):
+    """A function to calculate a tip"""
+    return round(float(bill_total) * float(tip_percentage), 2)
+
+# Testing that the above funtion works
+print("To calculate tip amount first,")
+print("enter your bill total:")
+bill_total = input()
+print("Now enter tip percentage in decimal form:")
+tip_percentage = input()
+print("Tip amount:")
+print("$", calculate_tip(bill_total, tip_percentage))
