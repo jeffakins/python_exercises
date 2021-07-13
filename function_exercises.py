e3b8c8e7ba14f352765558dc5313711a08fd0dda
Jeff Akins <jeffakins21@gmail.com>
2021-07-13 16:48:29 -0700

    Working through the import exercises

--- function_exercises.py
+++ function_exercises.py
@@ -11,9 +11,9 @@ def is_two(number):
         return False
 
 # Testing the function:
-print(is_two(2))
-print(is_two("2"))
-print(is_two(4))
+# print(is_two(2))
+# print(is_two("2"))
+# print(is_two(4))
 
 # 2. Define a function named is_vowel. 
 #    It should return True if the passed string is a vowel, False otherwise.
@@ -27,12 +27,12 @@ def is_vowel(letter): # Function to check if a letter is a vowel
     return False
 
 # User input. Will check if the input is a vowel using the above function.
-print("Input a vowel") 
-letter = input()
-if is_vowel(letter) == True:
-    print("That is a vowel")
-else:
-    print("Not a vowel")
+# print("Input a vowel") 
+# letter = input()
+# if is_vowel(letter) == True:
+#     print("That is a vowel")
+# else:
+#     print("Not a vowel")
 
 
 
@@ -48,12 +48,12 @@ def is_consonant(letter):
         return False
 
 # User input. Will check if the input is a consonant using the above function.
-print("Input a letter") 
-letter = input()
-if is_consonant(letter) == True:
-    print("That is a consonant")
-else:
-    print("Not a consonant")
+# print("Input a letter") 
+# letter = input()
+# if is_consonant(letter) == True:
+#     print("That is a consonant")
+# else:
+#     print("Not a consonant")
 
 
 # 4. Define a function that accepts a string that is a word. 
@@ -67,10 +67,10 @@ def cap_first_letter(word):
         return word
 
 # Testing that the above function works
-print("Type a word")
-word = input()
-print("Your word:")
-print(cap_first_letter(word))
+# print("Type a word")
+# word = input()
+# print("Your word:")
+# print(cap_first_letter(word))
 
 
 # 5. Define a function named calculate_tip. 
@@ -82,13 +82,13 @@ def calculate_tip(bill_total, tip_percentage):
     return round(float(bill_total) * float(tip_percentage), 2)
 
 # Testing that the above funtion works
-print("To calculate tip amount first,")
-print("enter your bill total:")
-bill_total = input()
-print("Now enter tip percentage in decimal form:")
-tip_percentage = input()
-print("Tip amount:")
-print("$", calculate_tip(bill_total, tip_percentage))
+# print("To calculate tip amount first,")
+# print("enter your bill total:")
+# bill_total = input()
+# print("Now enter tip percentage in decimal form:")
+# tip_percentage = input()
+# print("Tip amount:")
+# print("$", calculate_tip(bill_total, tip_percentage))
 
 
 # 6. Define a function named apply_discount. 
@@ -102,13 +102,13 @@ def apply_discount(original_price, discount_percent):
     return round(discount_price, 2)
 
 # Testing that the above funtion works
-print("To calculate the price of your item after applying a discount first,")
-print("enter the price of your item:")
-original_price = input()
-print("Now enter the discount percentage in decimal form:")
-discount_percent = input()
-print("Discounted price:")
-print("$", apply_discount(original_price, discount_percent))
+# print("To calculate the price of your item after applying a discount first,")
+# print("enter the price of your item:")
+# original_price = input()
+# print("Now enter the discount percentage in decimal form:")
+# discount_percent = input()
+# print("Discounted price:")
+# print("$", apply_discount(original_price, discount_percent))
 
 
 
@@ -121,8 +121,8 @@ def handle_commas(number):
     return float(num_no_comma)
 
 # Testing the above function
-print(handle_commas("3,254"))
-print(handle_commas("3,254,123,456"))
+# print(handle_commas("3,254"))
+# print(handle_commas("3,254,123,456"))
 
 
 
@@ -146,10 +146,10 @@ def get_letter_grade(numeric_grade):
 
 
 # Test above
-print(get_letter_grade(55))
-print(get_letter_grade(85))
-print(get_letter_grade(95))
-print(get_letter_grade(155))
+# print(get_letter_grade(55))
+# print(get_letter_grade(85))
+# print(get_letter_grade(95))
+# print(get_letter_grade(155))
 
 
 
@@ -165,9 +165,9 @@ def remove_vowels(word):
     return word
 
 # Test above
-print(remove_vowels("This is a test"))
-print(remove_vowels("Yo aeiou ya"))
-print(remove_vowels("Let's try this one more time"))
+# print(remove_vowels("This is a test"))
+# print(remove_vowels("Yo aeiou ya"))
+# print(remove_vowels("Let's try this one more time"))
 
 
 # 10. Define a function named normalize_name. 
@@ -188,7 +188,7 @@ def normalize_name(input_str):
     return norm_str
 
 # Test 10. 
-normalize_name("1 2 3 $()&^$( T#%^H#%^i%^s%^ i%^&S %&(*(a v$%&AlI#%^d p#%^YTh#%^on id#%^%&en$%&TI#$^%%$&^*Fi@$%^*(er  #^#^#@")
+#normalize_name("1 2 3 $()&^$( T#%^H#%^i%^s%^ i%^&S %&(*(a v$%&AlI#%^d p#%^YTh#%^on id#%^%&en$%&TI#$^%%$&^*Fi@$%^*(er  #^#^#@")
 
 
 
--- import_exercises.py
+++ import_exercises.py
@@ -6,10 +6,60 @@
 #    a. Run an interactive python session and import the module. 
 #       Call the is_vowel function using the . syntax.
 
+from function_exercises import is_vowel
+
+print(is_vowel("a"))
+
 #    b. Create a file named import_exericses.py. 
 #       Within this file, use from to import the calculate_tip function directly. 
 #       Call this function with values you choose and print the result.
 
+from function_exercises import get_letter_grade
+
+print(get_letter_grade(99))
+
 #    c. Create a jupyter notebook named import_exercises.ipynb. 
 #       Use from to import the get_letter_grade function and give it an alias. 
 #       Test this function in your notebook.
+
+# ** Complete **
+
+
+
+
+# 2. Read about and use the itertools module from the python 
+#    standard library to help you solve the following problems:
+
+#       a. How many different ways can you combine the letters from "abc" with the numbers 1, 2, and 3?
+
+from itertools import combinations
+print(list(combinations('ABC123', 6)))
+
+#       b. How many different combinations are there of 2 letters from "abcd"?
+
+from itertools import combinations
+print(list(combinations('ABCD', 2)))
+
+#       c. How many different permutations are there of 2 letters from "abcd"?
+
+from itertools import permutations
+print(list(permutations('ABCD', 2)))
+print(list(permutations('ABCD', 2)))
+
+
+
+# 3. Save this file as profiles.json inside of your exercises directory 
+#    (right click -> save file as...). 
+#    Use the load function from the json module to open this file.
+
+import json
+
+profiles = list(json.load(open('profiles.json'))) 
+#print(profiles[0])
+
+#   Total number of users
+print(len(profiles)) # Total number of users is 19
+
+#   Number of active users
+for profile in profiles:
+    if profile
