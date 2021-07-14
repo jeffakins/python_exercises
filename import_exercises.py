c191315b527ce51e9caeb81bc955bf3070b37f06
Jeff Akins <jeffakins21@gmail.com>
2021-07-14 10:44:00 -0700

    Last problem of import exercises

--- import_exercises.py
+++ import_exercises.py
@@ -32,20 +32,21 @@ print(get_letter_grade(99))
 
 #       a. How many different ways can you combine the letters from "abc" with the numbers 1, 2, and 3?
 
-from itertools import combinations
-print(list(combinations('ABC123', 6)))
+from itertools import product
+print(list(product('ABC', [1, 2, 3])))
+len(list(product('ABC', [1, 2, 3])))
 
 #       b. How many different combinations are there of 2 letters from "abcd"?
 
 from itertools import combinations
 print(list(combinations('ABCD', 2)))
+len(list(combinations('ABCD', 2)))
 
 #       c. How many different permutations are there of 2 letters from "abcd"?
 
 from itertools import permutations
 print(list(permutations('ABCD', 2)))
-print(list(permutations('ABCD', 2)))
-
+len(list(permutations('ABCD', 2)))
 
 
 # 3. Save this file as profiles.json inside of your exercises directory 
@@ -69,7 +70,7 @@ def number_active_users():
     return i
 
 print("Number of active users:")
-print(number_active_users())
+print(number_active_users()) # Number of active users: 9
 
 #   c. Number of inactive users
 
@@ -81,7 +82,7 @@ def number_inactive_users():
     return i
 
 print("Number of inactive users:")
-print(number_inactive_users())
+print(number_inactive_users()) # Number of inactive users: 10
 
 #   d. Grand total of balances for all users
 
@@ -96,7 +97,7 @@ def sum_balances():
     return total
 
 print("Total balances for all users:")
-print("$", sum_balances())
+print("$", sum_balances()) # Result: $ 52667.02
 
 #   e. Average balance per user
 
@@ -116,4 +117,86 @@ def avg_balance():
     return round(total / i, 2)
 
 print("Average balance for all users:")
-print("$", avg_balance())
+print("$", avg_balance()) # Result: $ 2771.95
+
+
+#   f. User with the lowest balance
+
+def lowest_balance():
+    lowest_balance = float("inf")
+    for profile in profiles:
+        temp_balance = profile["balance"] # created a variable to modify and keep things straight in my head
+        remove_comma_balance = temp_balance.replace(",", "") # removes commas from the string
+        remove_dollar_sign_balance = remove_comma_balance.replace("$", "") #Removes $ from the string
+        numeric_balance = float(remove_dollar_sign_balance) # transforms the balance into float type
+        if numeric_balance < lowest_balance:
+            lowest_balance = numeric_balance
+    return round(lowest_balance, 2)
+
+print("Lowest balance:")
+print("$", lowest_balance())
+
+#   g. User with the highest balance
+
+def highest_balance():
+    highest_balance = float("-inf")
+    for profile in profiles:
+        temp_balance = profile["balance"] # created a variable to modify and keep things straight in my head
+        remove_comma_balance = temp_balance.replace(",", "") # removes commas from the string
+        remove_dollar_sign_balance = remove_comma_balance.replace("$", "") #Removes $ from the string
+        numeric_balance = float(remove_dollar_sign_balance) # transforms the balance into float type
+        if numeric_balance > highest_balance:
+            highest_balance = numeric_balance
+    return round(highest_balance, 2)
+
+print("Highest balance:")
+print("$", highest_balance())
+
+#   h. Most common favorite fruit
+
+def favorite_fruit():
+    highest_fruit = ""
+    highest_count = 0
+    for profile in profiles:
+        fruit_counter = profile["favoriteFruit"]
+        fruit_count = 0
+        for compare in profiles:
+            if fruit_counter == compare["favoriteFruit"]:
+                fruit_count += 1
+        if fruit_count > highest_count:
+            highest_count = fruit_count
+            highest_fruit = fruit_counter
+    return highest_fruit
+
+print("Most common favorite fruit")
+print(favorite_fruit())
+
+#   i. Least most common favorite fruit
+
+def least_favorite_fruit():
+    highest_fruit = ""
+    highest_count = 0
+    for profile in profiles:
+        fruit_counter = profile["favoriteFruit"]
+        fruit_count = 0
+        for compare in profiles:
+            if fruit_counter == compare["favoriteFruit"]:
+                fruit_count += 1
+        if fruit_count > highest_count:
+            highest_count = fruit_count
+            highest_fruit = fruit_counter
+    return highest_fruit
+
+print("Most common favorite fruit")
+print(least_favorite_fruit())
+
+#   j. Total number of unread messages for all users
+
+def total_unread_messages():
+    for profile in profiles:
+        greeting = profile["greeting"]
+        num_messages = ""
+        for number in greeting:
+            if number.isdigit:
+                num_messages.append(number)
+        
