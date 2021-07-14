fc26cf85bb61dd56e0979191e0392bf12f8a55d2
Jeff Akins <jeffakins21@gmail.com>
2021-07-14 13:43:01 -0700

    Complete with import exercises, adding comments

--- import_exercises.py
+++ import_exercises.py
@@ -8,7 +8,7 @@
 
 from function_exercises import is_vowel
 
-print(is_vowel("a"))
+print(is_vowel("a"))                                # Returned True
 
 #    b. Create a file named import_exericses.py. 
 #       Within this file, use from to import the calculate_tip function directly. 
@@ -16,7 +16,7 @@ print(is_vowel("a"))
 
 from function_exercises import get_letter_grade
 
-print(get_letter_grade(99))
+print(get_letter_grade(99))                         # Returned A
 
 #    c. Create a jupyter notebook named import_exercises.ipynb. 
 #       Use from to import the get_letter_grade function and give it an alias. 
@@ -34,19 +34,19 @@ print(get_letter_grade(99))
 
 from itertools import product
 print(list(product('ABC', [1, 2, 3])))
-len(list(product('ABC', [1, 2, 3])))
+len(list(product('ABC', [1, 2, 3])))                # 9 combinations
 
 #       b. How many different combinations are there of 2 letters from "abcd"?
 
 from itertools import combinations
 print(list(combinations('ABCD', 2)))
-len(list(combinations('ABCD', 2)))
+len(list(combinations('ABCD', 2)))                  # 6 combinations
 
 #       c. How many different permutations are there of 2 letters from "abcd"?
 
 from itertools import permutations
 print(list(permutations('ABCD', 2)))
-len(list(permutations('ABCD', 2)))
+len(list(permutations('ABCD', 2)))                  # 12 permutations
 
 
 # 3. Save this file as profiles.json inside of your exercises directory 
@@ -55,11 +55,11 @@ len(list(permutations('ABCD', 2)))
 
 import json # Initialization
 
-profiles = list(json.load(open('profiles.json'))) 
+profiles = list(json.load(open('profiles.json')))   # Storing the json file into a list variable
 
 #   a. Total number of users
 print("Number of users:")
-print(len(profiles)) # Total number of users is 19
+print(len(profiles))                                # Total number of users is 19
 
 #   b. Number of active users
 def number_active_users():
@@ -70,7 +70,7 @@ def number_active_users():
     return i
 
 print("Number of active users:")
-print(number_active_users()) # Number of active users: 9
+print(number_active_users())                        # Number of active users: 9
 
 #   c. Number of inactive users
 
@@ -82,22 +82,21 @@ def number_inactive_users():
     return i
 
 print("Number of inactive users:")
-print(number_inactive_users()) # Number of inactive users: 10
+print(number_inactive_users())                      # Number of inactive users: 10
 
 #   d. Grand total of balances for all users
 
 def sum_balances():
     total = 0 # initialize total
     for profile in profiles:
-        # print("User balance:", profile["balance"]) # Used to check balances if needed
-        temp_balance = profile["balance"] # created a variable to modify and keep things straight in my head
-        remove_comma_balance = temp_balance.replace(",", "") # removes commas from the string
-        remove_dollar_sign_balance = remove_comma_balance.replace("$", "") #Removes $ from the string
-        total += float(remove_dollar_sign_balance) # Adds all balances in list
+        temp_balance = profile["balance"]                                   # Created a variable to modify and keep things straight in my head
+        remove_comma_balance = temp_balance.replace(",", "")                # Removes commas from the string
+        remove_dollar_sign_balance = remove_comma_balance.replace("$", "")  # Removes $ from the string
+        total += float(remove_dollar_sign_balance)                          # Adds all balances in list
     return total
 
 print("Total balances for all users:")
-print("$", sum_balances()) # Result: $ 52667.02
+print("$", sum_balances())                                                  # Result: $ 52667.02
 
 #   e. Average balance per user
 
@@ -105,19 +104,16 @@ def avg_balance():
     i = 0
     total = 0 # initialize total
     for profile in profiles:
-        # print("User balance:", profile["balance"]) # Used to check balances if needed
-        temp_balance = profile["balance"] # created a variable to modify and keep things straight in my head
-        remove_comma_balance = temp_balance.replace(",", "") # removes commas from the string
-        remove_dollar_sign_balance = remove_comma_balance.replace("$", "") #Removes $ from the string
-        total += float(remove_dollar_sign_balance) # Adds all balances in list
-        i += 1 # 19 balances
-    #print("len(profiles:", len(profiles)) # Test to see if lenth == i
-    #print("Total:", total) # Test for total
-    #print("i:", i ) # Test for i
-    return round(total / i, 2)
+        # print("User balance:", profile["balance"])                        # Used to check balances if needed
+        temp_balance = profile["balance"]                                   # Created a variable to modify and keep things straight in my head
+        remove_comma_balance = temp_balance.replace(",", "")                # Removes commas from the string
+        remove_dollar_sign_balance = remove_comma_balance.replace("$", "")  # Removes $ from the string
+        total += float(remove_dollar_sign_balance)                          # Adds all balances in list
+        i += 1                                                              # 19 balances
+    return round(total / i, 2)                                              
 
 print("Average balance for all users:")
-print("$", avg_balance()) # Result: $ 2771.95
+print("$", avg_balance())                                                   # Result: $ 2771.95
 
 
 #   f. User with the lowest balance
@@ -125,10 +121,10 @@ print("$", avg_balance()) # Result: $ 2771.95
 def lowest_balance():
     lowest_balance = float("inf")
     for profile in profiles:
-        temp_balance = profile["balance"] # created a variable to modify and keep things straight in my head
-        remove_comma_balance = temp_balance.replace(",", "") # removes commas from the string
-        remove_dollar_sign_balance = remove_comma_balance.replace("$", "") #Removes $ from the string
-        numeric_balance = float(remove_dollar_sign_balance) # transforms the balance into float type
+        temp_balance = profile["balance"]                                   # created a variable to modify and keep things straight in my head
+        remove_comma_balance = temp_balance.replace(",", "")                # removes commas from the string
+        remove_dollar_sign_balance = remove_comma_balance.replace("$", "")  #Removes $ from the string
+        numeric_balance = float(remove_dollar_sign_balance)                 # transforms the balance into float type
         if numeric_balance < lowest_balance:
             lowest_balance = numeric_balance
     return round(lowest_balance, 2)
@@ -141,10 +137,10 @@ print("$", lowest_balance())
 def highest_balance():
     highest_balance = float("-inf")
     for profile in profiles:
-        temp_balance = profile["balance"] # created a variable to modify and keep things straight in my head
-        remove_comma_balance = temp_balance.replace(",", "") # removes commas from the string
-        remove_dollar_sign_balance = remove_comma_balance.replace("$", "") #Removes $ from the string
-        numeric_balance = float(remove_dollar_sign_balance) # transforms the balance into float type
+        temp_balance = profile["balance"]                                   # created a variable to modify and keep things straight in my head
+        remove_comma_balance = temp_balance.replace(",", "")                # removes commas from the string
+        remove_dollar_sign_balance = remove_comma_balance.replace("$", "")  #Removes $ from the string
+        numeric_balance = float(remove_dollar_sign_balance)                 # transforms the balance into float type
         if numeric_balance > highest_balance:
             highest_balance = numeric_balance
     return round(highest_balance, 2)
@@ -155,21 +151,21 @@ print("$", highest_balance())
 #   h. Most common favorite fruit
 
 def favorite_fruit():
-    highest_fruit = ""
-    highest_count = 0
-    for profile in profiles:
-        fruit_counter = profile["favoriteFruit"]
-        fruit_count = 0
-        for compare in profiles:
-            if fruit_counter == compare["favoriteFruit"]:
-                fruit_count += 1
-        if fruit_count > highest_count:
-            highest_count = fruit_count
-            highest_fruit = fruit_counter
-    return highest_fruit
+    highest_fruit = ""                                      # Initializing str variable
+    highest_count = 0                                       # Initializing int variable
+    for profile in profiles:                                # Iterating through the list
+        fruit_counter = profile["favoriteFruit"]            # Variable to compare the below nested loop
+        fruit_count = 0                                     # Initializing int variable
+        for compare in profiles:                            # Nested loop to compare and count like fruit
+            if fruit_counter == compare["favoriteFruit"]:   # compare variables
+                fruit_count += 1                            # Count like fruit
+        if fruit_count > highest_count:                     # if statement to compare total amounts of fruits
+            highest_count = fruit_count                     # Stores number of fruit
+            highest_fruit = fruit_counter                   # Stores name of fruit
+    return highest_fruit                                    # Returns most common fruit
 
 print("Most common favorite fruit")
-print(favorite_fruit())
+print(favorite_fruit())                                     # Strawberry is the most common favorite fruit
 
 #   i. Least most common favorite fruit
 
@@ -193,10 +189,16 @@ print(least_favorite_fruit())
 #   j. Total number of unread messages for all users
 
 def total_unread_messages():
-    for profile in profiles:
-        greeting = profile["greeting"]
-        num_messages = ""
-        for number in greeting:
-            if number.isdigit:
-                num_messages.append(number)
-        
+    total_messages = 0                          # Initialize variabe
+    for profile in profiles:                    # Iterate through the list
+        # print(total_messages)                 # Used to print each iteration
+        greeting = profile["greeting"]          
+        num_messages = ""                       # Initialiing variable
+        for number in greeting:                 # Iterate through the string
+            if number.isdigit():                # Check for numbers within the string
+                num_messages += number          # Adding numbers to a new variable
+        total_messages += int(num_messages)     # Converting string numbers to int and summing
+    return total_messages                       # Output total
+
+print("Total number of unread messages:")
+print(total_unread_messages())                  # Total: 210 unread messages
